499c873f1e436c497d77df58f2c4eb10837efda7
M Nur Fauzan W <fauzan.widyanto1@gmail.com>
2018-11-15 09:55:32 +0700

    update Code for zakat

--- application/controllers/zakat_ctrl.php
+++ application/controllers/zakat_ctrl.php
@@ -14,24 +14,20 @@
             if($this->input->post('t1')=="" && $this->input->post('t2')==""){
                 $data['t1'] = date("01-m-Y");
                 $data['t2'] = date("t-m-Y");
-                $t1 = str_replace("-","",$data['t1']);
-                $t2 = str_replace("-","",$data['t2']);
-                $pieces1 = explode("-",$data['t1']);
-                $pieces2 = explode("-",$data['t2']);
-                $t1 = $pieces1[2].$pieces1[1].$pieces1[0];
-                $t2 = $pieces2[2].$pieces2[1].$pieces2[0];
+                $date1 = date_create($data['t1']);
+                $date2 = date_create($data['t2']);
+                $t1 = date_format($date1, "Ymd");
+                $t2 = date_format($date2, "Ymd");
                 $data['data'] = $this->Lap_zakat_model->sel_date($t1,$t2)->result();
                 $this->load->view('lap_zakat_view',$data);
             }
             else{
                 $data['t1'] = $this->input->post('t1');
                 $data['t2'] = $this->input->post('t2');
-                $t1 = str_replace("-","",$this->input->post('t1'));
-                $t2 = str_replace("-","",$this->input->post('t2'));
-                $pieces1 = explode("-",$data['t1']);
-                $pieces2 = explode("-",$data['t2']);
-                $t1 = $pieces1[2].$pieces1[1].$pieces1[0];
-                $t2 = $pieces2[2].$pieces2[1].$pieces2[0];
+                $date1 = date_create($data['t1']);
+                $date2 = date_create($data['t2']);
+                $t1 = date_format($date1, "Ymd");
+                $t2 = date_format($date2, "Ymd");
                 $data['data'] = $this->Lap_zakat_model->sel_date($t1,$t2)->result();
                 $this->load->view('lap_zakat_view',$data);
             }
