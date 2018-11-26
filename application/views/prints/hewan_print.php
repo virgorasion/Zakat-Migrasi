0ba61be04613216b00daeeab6505d60b22b97660
M Nur Fauzan W <fauzan.widyanto1@gmail.com>
2018-11-26 19:25:26 +0700

    add fiture print for menu kurban

--- application/views/prints/hewan_print.php
+++ application/views/prints/hewan_print.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Miftahul Jannah | Print</title>
-  
+
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!--jQuery-->
   <script src="<?php echo base_url('user_guide/_static/jquery-1.11.1.js')?>"></script>
@@ -16,28 +17,34 @@
   <link rel="stylesheet" href="<?php echo base_url('assets/ionicons-2.0.1/css/ionicons.min.css')?>">
   <!-- Theme style -->
   <link rel="stylesheet" href="<?php echo base_url('assets/AdminLTE-2.3.7/dist/css/AdminLTE.min.css')?>">
-  
+
   <link rel="stylesheet" href="<?php echo base_url('assets/AdminLTE-2.3.7/dist/css/skins/_all-skins.min.css')?>">
-  
+
 </head>
+
 <body onload="window.print();">
-<div class="container">
-  <!-- Main content -->
-  <br>
-  <b>Miftahul Jannah</b>
-  <br>Surabaya
-  <br>Kapas Madya
-  <br>083849575737
-  <center><h1 style="background-color: #D4D4D4"><b>LAPORAN HEWAN KURBAN</b></h1></center>
-  <center><h4 ><b><?php echo ' Periode : '.$t1 .' s/d '. $t2  ;?></b></h4></center>
-  
- <!-- Default box -->
-    <div class="box">        
-       
-       <!-- /.box-header -->
-        <div class="box-body">
-          <table id="datatable" class="table table-bordered table-hover">
-            <thead>
+  <div class="container">
+    <!-- Main content -->
+    <br>
+    <b>Miftahul Jannah</b>
+    <br>Surabaya
+    <br>Kapas Madya
+    <br>083849575737
+    <center>
+      <h1 style="background-color: #D4D4D4"><b>LAPORAN HEWAN KURBAN</b></h1>
+    </center>
+    <center>
+      <h4><b>
+          <?php echo ' Periode : '.$t1 .' s/d '. $t2  ;?></b></h4>
+    </center>
+
+    <!-- Default box -->
+    <div class="box">
+
+      <!-- /.box-header -->
+      <div class="box-body">
+        <table id="datatable" class="table table-bordered table-hover">
+          <thead>
             <tr>
               <th>No.</th>
               <th>Pembuat</th>
@@ -45,30 +52,50 @@
               <th>Alamat</th>
               <th>Jenis Hewan</th>
               <th>jumlah</th>
-              <th>Detail Waktu</th>
             </tr>
-            </thead>
-            <tbody>
-                <?php 
-        foreach ($data as $row): ?>
-                    <tr>                              
-                        <td class="no"><?php echo $row->nomor; ?></td>
-                        <td class="nama_cabang"><?php echo $row->nama; ?></td>
-                        <td class="no_faktur_jual"><?php echo $row->penyumbang;?></td>
-                        <td class="tanggal_transaksi"><?php echo $row->alamat;?></td>
-                        <td class="nama_kasir"><?php echo $row->jenis;?></td>
-                        <td><?php echo $row->jumlah ;?></td>
-                        <td><?php echo $row->log_time;?></td>
-                    </tr>
-                <?php 
-        endforeach
-        ?>
-            </tbody>
-          </table>
-        </div>
+          </thead>
+          <tbody>
+            <?php $no=1;
+        foreach ($data as $row){
+          switch ($row->jenis) {
+            case '1':
+              $jenis = "Kambing";
+              break;
+            case '2':
+              $jenis = "Sapi";
+              break;
+            default:
+              $jenis = "Undefined";
+              break;
+          } ?>
+            <tr>
+              <td class="no">
+                <?= $no ?>
+              </td>
+              <td class="pembuat">
+                <?= $row->admin; ?>
+              </td>
+              <td class="penyumbang">
+                <?= $row->penyumbang;?>
+              </td>
+              <td class="alamat">
+                <?= $row->alamat;?>
+              </td>
+              <td class="jenisHewan">
+                <?= $row->jenis;?>
+              </td>
+              <td class="jumlah">
+                <?= $row->jumlah ;?>
+              </td>
+            </tr>
+            <?php $no++; } ?>
+          </tbody>
+        </table>
+      </div>
     </div><!-- /.box -->
-  <!-- /.content -->
-</div>
-<!-- ./wrapper -->
+    <!-- /.content -->
+  </div>
+  <!-- ./wrapper -->
 </body>
+
 </html>
