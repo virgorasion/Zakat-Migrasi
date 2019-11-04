358aae558f0c933b790f72c485c9f34018f4dc23
Mas Agung Muhammad Ikhya Ulumuddin <nyambekgoreng89@gmail.com>
2019-11-04 13:50:39 +0700

    install SSL

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set('Asia/Jakarta');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://virgorasion.site/masjid/';
+$config['base_url'] = 'https://virgorasion.site/masjid/';
 
 /*
 |--------------------------------------------------------------------------
