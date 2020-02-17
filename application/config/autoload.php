e8cf0b26d2c6b42c19c994b9e137edc502364838
M Nur Fauzan W <fauzan.widyanto1@gmail.com>
2020-02-17 09:06:10 +0700

    add form validation library

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session','database','user_agent');
+$autoload['libraries'] = array('session','database','user_agent','form_validation');
 
 /*
 | -------------------------------------------------------------------
